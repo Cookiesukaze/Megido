f43c8acb9338aca06a18ce47f9b0bdecc6383ff1
Cookiesukaze <Cookiesukaze@qq.com>
2023-07-25 11:08:50 +0800

    提交了 图片转换为pdf 增加了文件类型筛选、文件起始路径为桌面、文件保存路径选择、文件保存不再为覆盖上一个文件等

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -1,27 +1,67 @@
 import img2pdf
-from PyQt5.QtWidgets import QApplication,QFileDialog
+from PyQt5.QtWidgets import QApplication, QFileDialog
+import os
+from tools import rename_path
 
 
-def convert_function(img_paths):
-    result = "spawn.pdf"
+def convert_function(img_paths, save_path):
+    result = save_path[0]+"/spawn.pdf"
+    result = rename_path.rename_path_function(result)
     with open(result, "wb") as f:
         f.write(img2pdf.convert(img_paths))
         return result
 
 
-def file_choose():
+def get_file_path():
+    desktop_path = get_desktop_path()
+    file_filter = "*jpeg *.jpg *.png"
     app = QApplication([])
     file_dialog = QFileDialog()
+    # 设置可选文件类型为存在的多个文件
     file_dialog.setFileMode(QFileDialog.ExistingFiles)
+    # 设置打开窗口名
+    file_dialog.setWindowTitle("选择一张或多张图片")
+    # 设置打开目录
+    file_dialog.setDirectory(desktop_path)
+    # 设置过滤器
+    file_dialog.setNameFilter(file_filter)
     if file_dialog.exec_():
         file_paths = file_dialog.selectedFiles()
         # file_paths = file_paths[::-1]
         # for file_path in file_paths:
         #     print(file_path)
-        print("多选文件列表：")
-        print("%s" % file_paths)
-        convert_function(file_paths)
+        # print("多选文件列表：")
+        # print("%s" % file_paths)
+        return file_paths
+
+
+def get_desktop_path():
+    return os.path.join(os.path.expanduser('~'), "Desktop")
+
+
+def get_save_path():
+    desktop_path = get_desktop_path()
+    app = QApplication([])
+    save_dialog = QFileDialog()
+    # 设置可选文件类型为目录
+    save_dialog.setFileMode(QFileDialog.Directory)
+    # 设置打开窗口名
+    save_dialog.setWindowTitle("选择保存路径")
+    # 设置打开目录
+    save_dialog.setDirectory(desktop_path)
+    if save_dialog.exec_():
+        save_path = save_dialog.selectedFiles()
+        # print(save_path)
+        return save_path
 
 
 if __name__ == "__main__":
-    file_choose()
+    convert_function(get_file_path(), get_save_path())
+
+'''
+1.起始路径确定 ✓
+2.文件类型筛选 ✓
+3.保存路径选择 ✓
+4.文件名选择 不想写，感觉没太大必要了
+5.图片质量选择
+'''
---
+++ tools/rename_path.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+
+def rename_path_function(path):
+    directory, file_name = os.path.split(path)
+    while os.path.isfile(path):
+        pattern = '(\d+)\)\.'
+        if re.search(pattern, file_name) is None:
+            file_name = file_name.replace('.', '(1).')
+        else:
+            current_number = int(re.findall(pattern, file_name)[-1])
+            new_number = current_number + 1
+            file_name = file_name.replace(f'({current_number}).', f'({new_number}).')
+        path = os.path.join(directory + os.sep + file_name)
+    return path
